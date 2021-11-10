b2bc7d87260761599582e1b753a08f511b414082
vysery98 <luispsi98@gmail.com>
2021-11-10 10:55:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/app01_pm/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.app01_pm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// TALLER - Utilizando clases, funciones y lo aprendido en clases realizar el siguiente algoritmo:
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 1. Validar si una persona es mayor o  menor de edad
+
+        // 2. Presentar la tabla de multiplicar de n número de forma ascendente y descendente
+
+        // 3. Mostrar el listado de paralelo de la materia de Plataformas móviles y los subgrupos por proyectos (Mostrar los resultados ordenados)
+
+        // 4. Presentar las propiedades de un vehículo utilizando clases, como tracción, motor, tipo de vehículo, capacidad
+
+        // 5. Algoritmo para ordenar un array
+
+        // 6. Validar un número de cédula
+
+    }
+}
